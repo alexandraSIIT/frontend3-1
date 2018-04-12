310333f448141e8c9cd3d94dcfd7bb4d52a65af6
Patraska Antonio <antoniopatraska@gmail.com>
2018-04-12 15:56:33 +0000

    Homepage

--- views/home.js
+++ views/home.js
@@ -3,44 +3,44 @@ window.addEventListener("load", function(){
    movies.getAllMovies('https://ancient-caverns-16784.herokuapp.com/movies/')
    .then(displayMovies);
     var container = document.getElementById("containermovies");
-    var template = document.getElementsByClassName("template")[0];
+     // var thumbnail = document.getElementsByClassName("col-lg-3 col-md-6 mb-4")[0];
+    
     var pagination = document.getElementsByClassName("pagination")[0];
 
     function displayMovies(moviesContent){
-    var currentPage = moviesContent.pagination.currentPage;
-    var allPages = moviesContent.pagination.numberOfPages;
-    document.getElementById("current-page").innerHTML = "<b>Current Page:" + currentPage + "/"+ allPages;
+    // var currentPage = moviesContent.pagination.currentPage;
+    // var allPages = moviesContent.pagination.numberOfPages;
+    // document.getElementById("current-page").innerHTML = "<b>Current Page:" + currentPage + "/"+ allPages;
     
-    var nextPage = moviesContent.pagination.links.next;
-    document.getElementById("next-page").innerHTML = "<a href=" + nextPage  + "/>" + "Next";
+    // var nextPage = moviesContent.pagination.links.next;
+    // document.getElementById("next-page").innerHTML = "<a href=" + nextPage  + "/>" + "Next";
     
     for (var i=0; i<moviesContent.results.length; i++){
+        
         displayElements(moviesContent.results[i]);
     }
 }
     
-    function displayElements(content){
-    var templateClone = template.cloneNode(true); 
-    var title = templateClone.querySelector("#title");
-    var genre = templateClone.querySelector("#genre");
-    var image = templateClone.querySelector("#img");
-    var rating = templateClone.querySelector("#rating");
-    var type = templateClone.querySelector("#type");
-    var deletemovie = templateClone.querySelector("#deleteButton");
-    var editmovie = templateClone.querySelector("#editButton");
-    image.innerHTML = "<img src=" + content.Poster+" />";
-    title.innerHTML= "<h2><a href=" + "https://myimbd-antoniopatraska.c9users.io/frontend3-1/pages/movieDetails.html?id=" + content._id +" />" + content.Title + " (" + content.Year + ")</h2>";
-    genre.innerHTML= "<b>Gender:</b> " + content.Genre;
-    rating.innerHTML="<b>Rating:</b> " + content.imdbRating;
-    type.innerHTML="<b>Type:</b> " + content.Type;
-    deletemovie.innerHTML =  "<button>Delete</button>" + "<button>Edit</button>" ;
-    // editmovie.innerHTML =  "<button>Edit</button>";
-    container.appendChild(templateClone);
     
-    var buttonDelete=document.getElementById("deleteButton");
-    buttonDelete.addEventListener("click",function(event){
-        movies.deleteMovie();
-    });
+    function displayElements(content){
+    var template = '<div class="col-md-4">';
+        template += '<div class="thumbnail">';
+        template += '<img class="img-test" + src='+content.Poster+' />';
+        template += '<div class="caption">';
+        template += "<h3><a href=" + "https://myimbd-antoniopatraska.c9users.io/frontend3-1/pages/movieDetails.html?id=" + content._id +">" + content.Title + " (" + content.Year + ") </a></h3>";
+        template += "<p><b>Gender:</b> " + content.Genre + "</p>";
+        template += "<p><b>Rating:</b>" + content.imdbRating + "</p>";
+        template += '<p><a href="#" class="btn btn-primary" role="button">Edit</a> <a href="#" class="btn btn-danger" role="button">Delete</a></p>';
+        template += "</div>";
+        template += "</div>";
+        template += "</div>";
+        template += "</div>";
+    container.innerHTML += template;
+    
+    // var buttonDelete=document.getElementById("deleteButton");
+    // buttonDelete.addEventListener("click",function(event){
+    //     movies.deleteMovie();
+    // });
     }
 });
 
